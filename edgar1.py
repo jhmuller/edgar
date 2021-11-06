ce9fd2b091c6ece38aa84b8c9cccf6cacbab35a5
John Muller <jmuller.ics88@gtalumni.org>
2021-11-06 08:46:28 -0400

    improvements

--- edgar1.py
+++ edgar1.py
@@ -64,7 +64,61 @@ def get_forms(df):
         url = f"https://www.sec.gov/Archives/" + ser["url"]
         resp = get_url_resp(url)
         print(resp.text)
+        res = read_html(resp.text)
         return resp.text
+
+def read_html(html):
+    import re
+    import pandas as pd
+    import xml.etree.ElementTree as ET
+    import io
+    try:
+        match = re.search(r"<\?xml", html)
+        start, end = match.span()
+        xmlstr = html[start:]
+        if False:
+            lines = html.split("\n")
+            for li, line in enumerate(lines):
+                if re.search("xml", line):
+                    break
+            xmllines = lines[li:]
+            xmlstr = '\n'.join(xmllines)
+
+
+        def iter_docs(author):
+            author_attr = author.attrib
+            for doc in author.iter('document'):
+                doc_dict = author_attr.copy()
+                doc_dict.update(doc.attrib)
+                doc_dict['data'] = doc.text
+                yield doc_dict
+
+        xml_data = io.StringIO(xmlstr)
+
+        etree = ET.parse(xml_data)  # create an ElementTree object
+        doc_df = pd.DataFrame(list(iter_docs(etree.getroot())))
+        x = pd.read_xml(xml)
+
+        DF_13F = pd.read_html(html)
+        DF_13F = DF_13F[-1]
+        DF_13F = DF_13F.iloc[2:]
+        new_header = DF_13F.iloc[0]
+
+        new_header = DF_13F.iloc[0]
+        DF_13F.columns = new_header
+        DF_13F = DF_13F.iloc[1:]
+        DF_13F['date_reported'] = datetime.now().strftime("%Y%m")
+        DF_13F['cik_company'] = cik_company
+        value_to_store_as_index = DF_13F['CUSIP'] + cik_company + datetime.now().strftime("%Y%m")
+        DF_13F['indice'] = value_to_store_as_index
+        DF_13F = DF_13F[
+            ['indice', 'NAME OF ISSUER', 'TITLE OF CLASS', 'CUSIP', '(x$1000)', 'PRN AMT', 'PRN', 'date_reported',
+             'cik_company']]
+        print('new SEC13 report added to DataFrame')
+        print(DF_13F)
+    except Exception as e:
+        print(sys.exc_info())
+        raise RuntimeException(e)
 
     def get_filings(urls):
         for url in urls:
